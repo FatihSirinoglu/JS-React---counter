0785a7c4b53efbf622d78560859fdaecdc8d815b
sSsiriusSs <m.fatih.abd@gmail.com>
2023-01-30 21:46:12 +0300

    biraz saçmalayıp 0 noktasına geldim, halloldu

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import Counter from './components/Counter';
-import './App.css';
-import PersonalChoices from './components/PersonalChoices';
+import React from 'react'
+import { useState } from 'react'
 
 
-function App(props) {
-  
-  
 
-  return (
-    <div className="App">
-      
-      <Counter />
-
-      <PersonalChoices />
-
-      
-    </div>
-  );
+function App() {
+    const [count, setCount]=useState(0)
+    const [input, setInput]=useState(1)
+    
+    return (
+        <div>
+          <div className='header'>
+            <h1>Kahrolası Counter a HojGeldiniz</h1>
+            <h2>{count}</h2>
+          </div>  
+          <div className='container'>
+            <input onChange={(event) => setInput(event.target.value)} placeholder="Kaçar artsın/azalsın?"></input>
+            <button onClick={()=>(setCount(count-(Number(input))))}>Decrease</button>
+            <button onClick={()=>(setCount(0))}>Reset</button>
+            <button onClick={()=>(setCount(count+(Number(input))))}>Increase</button>
+          </div>
+        </div>
+    )
 }
+
 
 export default App;
