57b1beadf86da7a0dd8562c6079aa318221240c4
sSsiriusSs <m.fatih.abd@gmail.com>
2023-01-30 21:52:53 +0300

    oneshot

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 
-
-
 function App() {
     const [count, setCount]=useState(0)
     const [input, setInput]=useState(1)
@@ -11,13 +9,15 @@ function App() {
         <div>
           <div className='header'>
             <h1>Kahrolası Counter a HojGeldiniz</h1>
-            <h2>{count}</h2>
+            <h1>{count}</h1>
           </div>  
           <div className='container'>
-            <input onChange={(event) => setInput(event.target.value)} placeholder="Kaçar artsın/azalsın?"></input>
-            <button onClick={()=>(setCount(count-(Number(input))))}>Decrease</button>
-            <button onClick={()=>(setCount(0))}>Reset</button>
-            <button onClick={()=>(setCount(count+(Number(input))))}>Increase</button>
+          <h1>{count}</h1>
+            <button className='btn' onClick={()=>(setCount(count-(Number(input))))}>Decrease</button>
+            <button className='btn reset' onClick={()=>(setCount(0))}>Reset</button>
+            <button className='btn' onClick={()=>(setCount(count+(Number(input))))}>Increase</button><br /><br />
+            <input className='input' onChange={(event) => setInput(event.target.value)} placeholder="Kaçar artsın/azalsın?"></input>
+            <h1>{count}</h1>
           </div>
         </div>
     )
