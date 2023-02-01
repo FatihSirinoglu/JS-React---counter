2b114cee813e4f4442dd9b6022dbcbab3ac4bf52
sSsiriusSs <m.fatih.abd@gmail.com>
2023-02-01 14:33:55 +0300

    add optional count value and stop/reset buttons

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { useState } from 'react'
 function App() {
     const [count, setCount]=useState(0)
     const [input, setInput]=useState(1)
-    
-    const handleClick = () => {
-      let intervalId;
+
+    const [autoInput, setAutoInput]=useState(1)
+
+    const [intervalId, setIntervalId]=useState(null)
+    // this state holds the changing value of count
+
+    const handleAutoClick = () => {
+      let intervalId
       if (count < 100) {
         intervalId = setInterval(() => {
           setCount(prevCount => {
@@ -14,11 +19,21 @@ function App() {
             clearInterval(intervalId);
             return 0;
           }
-          return prevCount + 1;
+          return Number(prevCount) + Number(autoInput);
         });  
         }, 1000);
       }
     };
+    const handleStopClick = () => {
+      clearInterval(intervalId);
+      return setCount(prevCount=>prevCount);
+    };
+    const handleResetClick = () => {
+      clearInterval(intervalId);
+      setIntervalId(null)
+    };
+
+    
     
     return (
         <div>
@@ -31,8 +46,17 @@ function App() {
             <button className='btn' onClick={()=>(setCount(count-(Number(input))))}>Decrease</button>
             <button className='btn reset' onClick={()=>(setCount(0))}>Reset</button>
             <button className='btn' onClick={()=>(setCount(count+(Number(input))))}>Increase</button><br /><br />
-            <input className='input' onChange={(event) => setInput(event.target.value)} placeholder="Kaçar artsın/azalsın?"></input><br /><br />
-            <button className='auto' onClick={handleClick}>Start Auto Count</button>
+            <input className='input' onChange={(event) => setInput(event.target.value)} placeholder="Kaçar artsın/azalsın?"></input><br /><br /> 
+          </div>
+          <div className='auto'>
+            <button className='auto' onClick={handleAutoClick}>Start Auto Count</button><br /><br />
+            <input className='input' placeholder='Oto Count Value' onChange={(e)=>setAutoInput(e.target.value)}></input> <br /><br />
+            <button className='btn' onClick={handleStopClick} >Stop</button> 
+            <button className='btn' onClick={handleResetClick}>Reset</button>
+          </div>
+
+          <div className='footer'>
+
           </div>
         </div>
     )
